2542db782cfd2874d6d5efacb3b58d5d2f3b32e3
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-22 23:27:09 +0100

    add valid guesses

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,14 +1,17 @@
 import { WORDS } from '../constants/wordlist'
+import { VALID_GUESSES } from '../constants/validGuesses'
 import { WRONG_SPOT_MESSAGE, NOT_CONTAINED_MESSAGE } from '../constants/strings'
 import { getGuessStatuses } from './statuses'
 import { default as GraphemeSplitter } from 'grapheme-splitter'
 
 export const isWordInWordList = (word: string) => {
-  return WORDS.includes(localeAwareUpperCase(word))
+  return (
+    WORDS.includes(localeAwareLowerCase(word)) ||
+    VALID_GUESSES.includes(localeAwareLowerCase(word))
+  )
 }
 
 export const isWinningWord = (word: string) => {
-  console.log({ solution })
   return solution === word
 }
 
