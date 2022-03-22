11c2e021669a46ef253ed07f52f424a9183176e2
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-22 01:26:28 +0100

    add link of the app in the share info

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -21,7 +21,8 @@ export const shareStatus = (
     `${GAME_TITLE} ${solutionIndex} ${
       lost ? 'X' : guesses.length
     }/${MAX_CHALLENGES}${isHardMode ? '*' : ''}\n\n` +
-    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode))
+    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode)) +
+    '\n\nhttps://ijambo.vercel.app/'
 
   const shareData = { text: textToShare }
 
