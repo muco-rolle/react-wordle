34e5866a21421dbd120a9ac44a7b36c32c8af358
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-23 11:43:07 +0100

    Revert "remove settings"
    This reverts commit a8651a9f5f6b7d989148a3646a39db876bff1950.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,8 @@
-import { ChartBarIcon, InformationCircleIcon } from '@heroicons/react/outline'
+import {
+  ChartBarIcon,
+  CogIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/outline'
 import { GAME_TITLE } from '../../constants/strings'
 
 type Props = {
@@ -25,6 +29,10 @@ export const Navbar = ({
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
             onClick={() => setIsStatsModalOpen(true)}
           />
+          {/* <CogIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsSettingsModalOpen(true)}
+          /> */}
         </div>
       </div>
       <hr></hr>
