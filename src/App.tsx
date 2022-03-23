80117d8c4fd982e0cc7bb77228f9825de37b9dab
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-23 23:18:42 +0100

    improve ui

--- src/App.tsx
+++ src/App.tsx
@@ -256,7 +256,7 @@ function App() {
         setIsStatsModalOpen={setIsStatsModalOpen}
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
-      <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
+      <div className="pt-24 px-1 pb-4 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
         <div className="pb-6 grow">
           <Grid
             guesses={guesses}
