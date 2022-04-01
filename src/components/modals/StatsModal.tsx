ee13b50d759dcba12c9e91f84932495cdeb5abdb
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-04-01 12:50:46 +0200

    display word of today only when you lost

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -65,38 +65,38 @@ export const StatsModal = ({
         gameStats={gameStats}
         numberOfGuessesMade={numberOfGuessesMade}
       />
+      {isGameLost && (
+        <p className="text-s leading-6 font-medium text-gray-900 dark:text-gray-100">
+          {CORRECT_WORD_MESSAGE(solution)}
+        </p>
+      )}
       {(isGameLost || isGameWon) && (
-        <>
-          <p className="text-s leading-6 font-medium text-gray-900 dark:text-gray-100">
-            {CORRECT_WORD_MESSAGE(solution)}
-          </p>
-          <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
-            <div>
-              <h5>{NEW_WORD_TEXT}</h5>
-              <Countdown
-                className="text-lg font-medium text-gray-900 dark:text-gray-100"
-                date={tomorrow}
-                daysInHours={true}
-              />
-            </div>
-            <button
-              type="button"
-              className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-              onClick={() => {
-                shareStatus(
-                  guesses,
-                  isGameLost,
-                  isHardMode,
-                  isDarkMode,
-                  isHighContrastMode,
-                  handleShareToClipboard
-                )
-              }}
-            >
-              {SHARE_TEXT}
-            </button>
+        <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
+          <div>
+            <h5>{NEW_WORD_TEXT}</h5>
+            <Countdown
+              className="text-lg font-medium text-gray-900 dark:text-gray-100"
+              date={tomorrow}
+              daysInHours={true}
+            />
           </div>
-        </>
+          <button
+            type="button"
+            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            onClick={() => {
+              shareStatus(
+                guesses,
+                isGameLost,
+                isHardMode,
+                isDarkMode,
+                isHighContrastMode,
+                handleShareToClipboard
+              )
+            }}
+          >
+            {SHARE_TEXT}
+          </button>
+        </div>
       )}
     </BaseModal>
   )
